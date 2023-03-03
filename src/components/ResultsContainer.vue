db1786088757f6d5db84a76f06cbbe3950d76694
Jason Tseng <jtseng@dotdash.com>
2023-03-03 12:52:50 -0500

    remove whitespace

--- src/components/ResultsContainer.vue
+++ src/components/ResultsContainer.vue
@@ -1,8 +1,7 @@
 <template>
     <div class="results-container" >
         <h2 class="results-container__title">Your Inspiration:</h2>
-        <div v-show="loading" class="spinner">
-        </div>
+        <div v-show="loading" class="spinner"></div>
         <div v-show="loaded" class="results-image">
             <img :src="inspirationUrl" >
         </div>
